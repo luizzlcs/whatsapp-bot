5edc2b0eb0ffc20531cb07ae29031a45151e7ee9
Luiz Carlos <luizzlcs@gmail.com>
2025-04-08 09:39:15 -0300

    Conversão de data e hora

---
+++ utils.js
@@ -0,0 +1,26 @@
+function formatarDataHora(date) {
+    const dia = String(date.getDate()).padStart(2, '0');
+    const mes = String(date.getMonth() + 1).padStart(2, '0');
+    const ano = date.getFullYear();
+    const horas = String(date.getHours()).padStart(2, '0');
+    const minutos = String(date.getMinutes()).padStart(2, '0');
+    const segundos = String(date.getSeconds()).padStart(2, '0');
+
+    return `${dia}/${mes}/${ano} ${horas}:${minutos}:${segundos}`;
+}
+
+function formatarNomeArquivoData(date) {
+    const dia = String(date.getDate()).padStart(2, '0');
+    const mes = String(date.getMonth() + 1).padStart(2, '0');
+    const ano = date.getFullYear();
+    const horas = String(date.getHours()).padStart(2, '0');
+    const minutos = String(date.getMinutes()).padStart(2, '0');
+    const segundos = String(date.getSeconds()).padStart(2, '0');
+
+    return `${dia}-${mes}-${ano}_${horas}-${minutos}-${segundos}`;
+}
+
+module.exports = {
+    formatarDataHora,
+    formatarNomeArquivoData
+};
